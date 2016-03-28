92f1f5d1c0cd31bdd06311b7b70bf73bef822050
Phil Skaggs <prskaggsjr@gmail.com>
2016-03-27 20:03:49 -0600

    add retweet of mindful/minfuness hashtags

--- bot.js
+++ bot.js
@@ -1,1 +1,28 @@
 var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         'geTSMthBY5UYIsNSJC03MRNNw',
+  consumer_secret:      'CwFLZSYKvnk1fRc4GCicavwT6ljlaktsjdWBy2NdG9PVBpuQye',
+  access_token:         '714252656504508416-aqJmsxEQTPakJAQI75tdZfr4Miginhz',
+  access_token_secret:  'k2UwHnDZdyP11ysv03pfnPlJaZaY4U2QEDYXA39ld1DIQ',
+})
+
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+//   console.log('working');
+// })
+
+// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+//   console.log(data)
+// })
+
+function retweetRecent() {T.get('search/tweets', {q: "#mindful OR #mindfulness", result_type: "recent"}, function (err, data, response) {
+  if (!err) {
+    var retweetId = data.statuses[0].id_str;
+    T.post('statuses/retweet/' + retweetId, { }, function (err, response) {
+      if (response) { console.log('Retweeted Tweet ID: ' + retweetId); }
+        if (err) { console.log('Retweet Error: ', err); } }); }
+        else { console.log('Search Error: ', err); } });}
+
+retweetRecent();
+setInterval(retweetRecent, 1800000);

e3b0cfe06dc2d184a5ccec83d0b3706055016a1f
Phil Skaggs <prskaggsjr@gmail.com>
2016-03-27 20:04:16 -0600

    delete unwanted code

--- bot.js
+++ bot.js
@@ -7,15 +7,6 @@ var T = new Twit({
   access_token_secret:  'k2UwHnDZdyP11ysv03pfnPlJaZaY4U2QEDYXA39ld1DIQ',
 })
 
-// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-//   console.log(data)
-//   console.log('working');
-// })
-
-// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-//   console.log(data)
-// })
-
 function retweetRecent() {T.get('search/tweets', {q: "#mindful OR #mindfulness", result_type: "recent"}, function (err, data, response) {
   if (!err) {
     var retweetId = data.statuses[0].id_str;
